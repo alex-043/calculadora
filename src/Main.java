bb23d51b4b571d9d5bf331b470b078cd7ce6e846
unknown <DAM1B-18@JOVELLANOS.EDU>
2024-03-07 19:50:01 +0100

    Subida de main inicial

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Calculadora");
+
+        double num1 = 0;
+        double num2 = 0;
+
+        System.out.println("Escribe el primer numero");
+        num1 = sc.nextDouble();
+
+        System.out.println("------Menu------");
+
+
+    }
+}
