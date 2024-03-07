3e12137841d2e2357e0692d66560a5c50594626b
Alex Hernandez <ah725604@gmail.com>
2024-03-07 22:23:35 +0100

    Subida de main inicialv4

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,27 @@ public class Main {
         System.out.println("0.Salir");
         System.out.println("--------------------");
         System.out.println("Escoge una opcion");
+        String opcion = sc.nextLine();
+
+        switch(opcion){
+            case "1":
+                System.out.println("Suma");
+                break;
+            case "2":
+                System.out.println("Resta");
+                break;
+            case "3":
+                System.out.println("Multiplicar");
+                break;
+            case "4":
+                System.out.println("Dividir");
+                break;
+            case "0":
+                break;
+            default:
+                System.out.println("Opcion incorrecta");
+                break;
+        } while (opcion != "0");
 
 
     }
