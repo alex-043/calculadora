465099365519c0aa802d7f0ed42bc0d4f53b3192
alex-043 <ah725604@gmail.com>
2024-03-07 23:12:33 +0100

    Subida de la clase suma v2

--- src/Suma.java
+++ src/Suma.java
@@ -1,8 +1,31 @@
 public class Suma {
 
-    public static double suma(double num, double num2){
-        return (num + num2);
+    private double num;
+    private double num2;
+
+    public Suma(double num, double num2){
+        this.num = num;
+        this.num2 = num2;
     }
 
+    public void setNum(double num) {
+        this.num = num;
+    }
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public double getNum() {
+        return num;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    @Override
+    public String toString(){
+        return "Suma: " + (num + num2);
+    }
 
 }
